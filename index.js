787d84978dd8dc5abf05b44adc7fa3f6fbf00de0
RuanPablo-7 <ToyaMON777@outlook.com>
2025-02-19 00:59:07 -0300

    update button completed edit delete

--- index.js
+++ index.js
@@ -10,58 +10,116 @@ buttonAdd.addEventListener('click', () => {
         alert('Por favor digite alguma tarefa!')
 
     } else {
-
         let text = input.value.trim()
         listas.push(text)
         createList(text)
     }
 })
 
-function createList(text) {
-    let List = document.getElementById('to-do-list')
-    let ListClass = document.querySelectorAll('.to-do-list')
-    let li = document.createElement('div')
-    li.classList.add('list')
+
+let list = document.getElementById('to-do-list')
+let li = document.createElement('div')
+li.classList.add('list')
+
+listas.forEach((item, index) => {
     
-    listas.forEach((element, indexx) => {
-        console.log('f',element, indexx)
-        li.innerHTML = ` 
-        <p class="p-list">${element}</p>
-        <div class="elements">
-        <button id="completed">C</button>
-        <button id="edit">E</button>
-        <button id="delete">D</button>
-        </div>
+        let pText = document.createElement('p')
+        pText.classList.add('p-list')
+        pText.innerHTML = item
+    
+        let divElements = document.createElement('div')
+        divElements.classList.add('elements')
         
-        `
-        List.appendChild(li)
-
-        let lis = document.querySelectorAll('.list')
-        lis.forEach((element, index) => {
-            element.addEventListener('click', () => {
-                
-                let bdelete = document.getElementById('delete')
-                console.log('dele', listas)
-
-                bdelete.addEventListener('click', () => {
-                   alert('Deletado')
-                })
-
-               // console.log(element, index)
-            })
-        })
-        console.log(listas)
-
-
+        let buttonCompleted = document.createElement('button')
+        let buttonEdit = document.createElement('button')
+        let buttonDelete = document.createElement('button')
+        
+        buttonCompleted.innerHTML = 'C'
+        buttonEdit.innerHTML = 'E'
+        buttonDelete.innerText = 'D'
+        
+        divElements.appendChild(buttonCompleted)
+        divElements.appendChild(buttonEdit)
+        divElements.appendChild(buttonDelete)
+        
+        li.appendChild(pText)
+        li.appendChild(divElements)
+        list.appendChild(li)
 
 })
+
+
+
+function createList(text) {
+    let list = document.getElementById('to-do-list')
+    let li = document.createElement('div')
+    li.classList.add('list')
+
+
+        let pText = document.createElement('p')
+        pText.classList.add('p-list')
+        pText.innerHTML = listas[listas.length - 1]
+
+        let divElements = document.createElement('div')
+        divElements.classList.add('elements')
         
-    
-    
-    
-  
+        let buttonCompleted = document.createElement('button')
+        let buttonEdit = document.createElement('button')
+        let buttonDelete = document.createElement('button')
+        
+        buttonCompleted.innerHTML = 'C'
+        buttonEdit.innerHTML = 'E'
+        buttonDelete.innerText = 'D'
+        
+        divElements.appendChild(buttonCompleted)
+        divElements.appendChild(buttonEdit)
+        divElements.appendChild(buttonDelete)
+        
+        li.appendChild(pText)
+        li.appendChild(divElements)
+        list.appendChild(li)
+
+        let controllButtonCompleted = true
+        buttonCompleted.addEventListener('click', () => {
+            if (controllButtonCompleted == true) {
+                list.querySelectorAll('.list').forEach((item, index) => {
+                    item.addEventListener('click', () => {
+                      pText.style.textDecoration = 'line-through'
+                    })
+                })
+                controllButtonCompleted = false
+                console.log(controllButtonCompleted)
+
+            } else if(controllButtonCompleted === false) {
+                list.querySelectorAll('.list').forEach((item, index) => {
+                    item.addEventListener('click', () => {
+                      pText.style.textDecoration = 'none'
+                    })
+                })
+                controllButtonCompleted = true
+                console.log(controllButtonCompleted)
+            }
+            //alert('Tarefa concluída!')
+            
+            
+            
+        })
+        buttonEdit.addEventListener('click', () => {
+            alert('Tarefa concluída!')
+        })
+        buttonDelete.addEventListener('click', () => {
+            
+            list.querySelectorAll('.list').forEach((item, index) => {
+                item.addEventListener('click', () => {
+                    console.log(index)
+                    listas.splice(index, 1)
+                    item.remove()
+                    console.log(listas)
+                })
+            })
+            //alert('Tarefa concluída!')
+        })
+   console.log(listas[listas.length - 1])
 
 }
-
-
 
---
+++ test.js
@@ -0,0 +1,5 @@
+let nome = ['carlos', 'joao', 'maria', 'jose', 'pedro']
+
+
+let nome2 = nome.splice(1,1)
+console.log(nome2)
